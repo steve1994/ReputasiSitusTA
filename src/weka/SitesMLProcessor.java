dc34a42da656da00ff5dfe8a0d7630df5946211b
steve1994 <steve.harnadi@gmail.com>
2016-05-30 15:02:24 +0700

    Add extract features SitesMLProcessor

--- src/weka/SitesMLProcessor.java
+++ src/weka/SitesMLProcessor.java
@@ -1,13 +1,22 @@
 package weka;
 
+import Utils.API.WOT_API_Loader;
+import Utils.DNS.DNSExtractor;
+import Utils.Spesific.ContentExtractor;
+import data_structure.feature.DNS_Feature;
+import data_structure.feature.Spesific_Feature;
+import data_structure.feature.Trust_Feature;
 import data_structure.instance_ML.SiteRecordReputation;
 import org.javatuples.Pair;
+import org.javatuples.Sextet;
+import sample.StaticVars;
 import weka.core.Attribute;
 import weka.core.FastVector;
 import weka.core.Instance;
 import weka.core.Instances;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Enumeration;
 import java.util.List;
 
@@ -235,5 +244,102 @@ public class SitesMLProcessor {
         }
 
         return vectorPercentageEachAttribute;
+    }
+
+    /**
+     * Extract a domainName's reputation features based on typeReputation
+     * @param domainName
+     * @param typeReputation
+     * @return
+     */
+    public static SiteRecordReputation extractFeaturesFromDomain(String domainName, int typeReputation) {
+        DNS_Feature fiturDNS = new DNS_Feature();
+
+        // DNS FEATURES
+        if (Arrays.asList(1, 4, 5, 7).contains(typeReputation)) {
+            // TLD ratio
+            Sextet<Double, Double, Double, Double, Double, Double> TLDRatio = DNSExtractor.getTLDDistributionFromAS(domainName);
+            Double[] TLDRatioList = new Double[6];
+            TLDRatioList[0] = TLDRatio.getValue0();
+            TLDRatioList[1] = TLDRatio.getValue1();
+            TLDRatioList[2] = TLDRatio.getValue2();
+            TLDRatioList[3] = TLDRatio.getValue3();
+            TLDRatioList[4] = TLDRatio.getValue4();
+            TLDRatioList[5] = TLDRatio.getValue5();
+            fiturDNS.setPopularTLDRatio(TLDRatioList);
+            System.out.println("TLD Ratio");
+
+            // Hit AS Ratio (malware, phishing, spamming)
+            Double[] HitRatioList = new Double[3];
+            for (int j = 0; j < 3; j++) {
+                HitRatioList[j] = DNSExtractor.getHitASRatio(domainName, j + 1);
+            }
+            fiturDNS.setHitASRatio(HitRatioList);
+            System.out.println("Hit AS Ratio");
+
+            // Name server distribution AS
+            fiturDNS.setDistributionNSAS(DNSExtractor.getDistributionNSFromAS(domainName));
+            System.out.println("Name Server Distribution AS");
+
+            // Name server count
+            fiturDNS.setNumNameServer(DNSExtractor.getNumNameServers(domainName));
+            System.out.println("Name Server Count");
+
+            // TTL Name Servers
+            fiturDNS.setListNSTTL(DNSExtractor.getNameServerTimeToLive(domainName));
+            System.out.println("TTL Name Servers");
+
+            // TTL DNS A Records
+            fiturDNS.setListDNSRecordTTL(DNSExtractor.getDNSRecordTimeToLive(domainName));
+            System.out.println("TTL DNS Record");
+        }
+
+        // SPESIFIC FEATURES
+        Spesific_Feature fiturSpesific = new Spesific_Feature();
+
+        if (Arrays.asList(2,4,6,7).contains(typeReputation)) {
+            // Token Count URL
+            fiturSpesific.setTokenCountURL(ContentExtractor.getDomainTokenCountURL(domainName));
+            System.out.println("Token Count URL");
+
+            // Average Token Length URL
+            fiturSpesific.setAverageTokenLengthURL(ContentExtractor.getAverageDomainTokenLengthURL(domainName));
+            System.out.println("Average Token Length URL");
+
+            // SLD ratio from URL (malware, phishing, spamming)
+            double[] SLDRatioList = new double[3];
+            for (int j = 0; j < 3; j++) {
+                SLDRatioList[j] = ContentExtractor.getSLDHitRatio(domainName, j + 1);
+            }
+            fiturSpesific.setSLDRatio(SLDRatioList);
+            System.out.println("SLD Ratio List");
+
+            // Inbound link Approximation (Google, Yahoo, Bing)
+            int[] inboundLinkAppr = new int[3];
+            for (int j = 0; j < 3; j++) {
+                inboundLinkAppr[j] = ContentExtractor.getInboundLinkFromSearchResults(domainName, j + 1);
+            }
+            fiturSpesific.setInboundLink(inboundLinkAppr);
+            System.out.println("Inbound Link Approximation");
+
+            // Lookup time to access site
+            fiturSpesific.setLookupTime(ContentExtractor.getDomainLookupTimeSite(domainName));
+            System.out.println("Lookup Time");
+        }
+
+        // TRUST FEATURES
+        Trust_Feature fiturTrust = new Trust_Feature();
+        if (Arrays.asList(3, 5, 6, 7).contains(typeReputation)) {
+            fiturTrust = WOT_API_Loader.loadAPIWOTForSite(domainName);
+            System.out.println("Trust WOT");
+        }
+
+        // SET RECORD INSTANCE DATA STRUCTURE
+        SiteRecordReputation recordML = new SiteRecordReputation();
+        recordML.setDNSRecordFeature(fiturDNS);
+        recordML.setSpesificRecordFeature(fiturSpesific);
+        recordML.setTrustRecordFeature(fiturTrust);
+
+        return recordML;
     }
 }
