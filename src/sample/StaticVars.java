1d38144a3ba91ba178f145ed02c4387664d684f7
steve1994 <steve.harnadi@gmail.com>
2016-05-30 15:04:23 +0700

    Add new attribute StaticVars JavaFX

--- src/sample/StaticVars.java
+++ src/sample/StaticVars.java
@@ -1,5 +1,11 @@
 package sample;
 
+import data_structure.instance_ML.SiteRecordReputation;
+import data_structure.instance_ML.historySitesReputation;
+import org.javatuples.Triplet;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -9,4 +15,9 @@ public class StaticVars {
     public static List<String> listSitesTraining;
     public static int numSitesTraining;
     public static int reputationType;
+    public static int methodType;
+    public static HashMap<String,historySitesReputation> historyReputation = new HashMap<String, historySitesReputation>();
+    public static String currentDomainName;
+    public static String currentLabel;
+    public static Triplet<Double,Double,Double> currentComposition;
 }
