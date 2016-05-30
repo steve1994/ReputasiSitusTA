047b8892470cacacc9b8a054f1f663da22a7bf07
steve1994 <steve.harnadi@gmail.com>
2016-05-30 11:18:55 +0700

    Create interface for real reputation system ; Add css file and its resource

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -19,68 +19,90 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
-    @FXML private RadioButton malwareRadioButton;
-    @FXML private RadioButton phishingRadioButton;
-    @FXML private RadioButton spammingRadioButton;
-    @FXML private RadioButton popularRadioButton;
-    @FXML private TextField numSitesTextField;
-    @FXML private CheckBox optionDNSCheckBox;
-    @FXML private CheckBox optionSpesificCheckBox;
-    @FXML private CheckBox optionTrustCheckBox;
-    private final ToggleGroup siteListRadioButton;
-
+    public RadioButton supervisedRadioButton;
+    public RadioButton unsupervisedRadioButton;
+    public RadioButton hybridRadioButton;
+    public RadioButton trustRadioButton;
+    public RadioButton spesificRadioButton;
+    public RadioButton dnsRadioButton;
+    public RadioButton dnsSpesificRadioButton;
+    public RadioButton dnsTrustRadioButton;
+    public RadioButton spesificTrustRadioButton;
+    public RadioButton dnsSpesificTrustRadioButton;
+    private final ToggleGroup methodRadioButton;
+    private final ToggleGroup featuresRadioButton;
 
     public Controller() {
-        siteListRadioButton = new ToggleGroup();
+        methodRadioButton = new ToggleGroup();
+        featuresRadioButton = new ToggleGroup();
     }
 
 
-    public void handleTrainingDataFeatures(ActionEvent actionEvent) {
-        // Setting static vars
-        StaticVars.numSitesTraining = Integer.parseInt(numSitesTextField.getText());
-        if (phishingRadioButton.isSelected()) {
-            StaticVars.listSitesTraining = EksternalFile.loadSitesTrainingList(2).getKey();
-        } else if (spammingRadioButton.isSelected()) {
-            StaticVars.listSitesTraining = EksternalFile.loadSitesTrainingList(3).getKey();
-        } else if (popularRadioButton.isSelected()) {
-            StaticVars.listSitesTraining = EksternalFile.loadSitesTrainingList(4).getKey();
-        } else {
-            StaticVars.listSitesTraining = EksternalFile.loadSitesTrainingList(1).getKey();
-        }
-        if (optionDNSCheckBox.isSelected() && (!optionSpesificCheckBox.isSelected()) && (!optionTrustCheckBox.isSelected())) {
-            StaticVars.reputationType = 1;
-        } else if (!optionDNSCheckBox.isSelected() && (optionSpesificCheckBox.isSelected()) && (!optionTrustCheckBox.isSelected())) {
-            StaticVars.reputationType = 2;
-        } else if (!optionDNSCheckBox.isSelected() && (!optionSpesificCheckBox.isSelected()) && (optionTrustCheckBox.isSelected())) {
-            StaticVars.reputationType = 3;
-        } else if (optionDNSCheckBox.isSelected() && (optionSpesificCheckBox.isSelected()) && (!optionTrustCheckBox.isSelected())) {
-            StaticVars.reputationType = 4;
-        } else if (optionDNSCheckBox.isSelected() && (!optionSpesificCheckBox.isSelected()) && (optionTrustCheckBox.isSelected())) {
-            StaticVars.reputationType = 5;
-        } else if (optionDNSCheckBox.isSelected() && (optionSpesificCheckBox.isSelected()) && (!optionTrustCheckBox.isSelected())) {
-            StaticVars.reputationType = 6;
-        } else {
-            StaticVars.reputationType = 7;
-        }
+//    @FXML private RadioButton malwareRadioButton;
+//    @FXML private RadioButton phishingRadioButton;
+//    @FXML private RadioButton spammingRadioButton;
+//    @FXML private RadioButton popularRadioButton;
+//    @FXML private TextField numSitesTextField;
+//    @FXML private CheckBox optionDNSCheckBox;
+//    @FXML private CheckBox optionSpesificCheckBox;
+//    @FXML private CheckBox optionTrustCheckBox;
+//    private final ToggleGroup siteListRadioButton;
 
-        // Go into Statistic screens
-        Parent root = null;
-        try {
-            root = FXMLLoader.load(getClass().getResource("statistik.fxml"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        Stage stage = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
-        stage.setScene(new Scene(root));
-    }
+//    public void handleTrainingDataFeatures(ActionEvent actionEvent) {
+//        // Setting static vars
+//        StaticVars.numSitesTraining = Integer.parseInt(numSitesTextField.getText());
+//        if (phishingRadioButton.isSelected()) {
+//            StaticVars.listSitesTraining = EksternalFile.loadSitesTrainingList(2).getKey();
+//        } else if (spammingRadioButton.isSelected()) {
+//            StaticVars.listSitesTraining = EksternalFile.loadSitesTrainingList(3).getKey();
+//        } else if (popularRadioButton.isSelected()) {
+//            StaticVars.listSitesTraining = EksternalFile.loadSitesTrainingList(4).getKey();
+//        } else {
+//            StaticVars.listSitesTraining = EksternalFile.loadSitesTrainingList(1).getKey();
+//        }
+//        if (optionDNSCheckBox.isSelected() && (!optionSpesificCheckBox.isSelected()) && (!optionTrustCheckBox.isSelected())) {
+//            StaticVars.reputationType = 1;
+//        } else if (!optionDNSCheckBox.isSelected() && (optionSpesificCheckBox.isSelected()) && (!optionTrustCheckBox.isSelected())) {
+//            StaticVars.reputationType = 2;
+//        } else if (!optionDNSCheckBox.isSelected() && (!optionSpesificCheckBox.isSelected()) && (optionTrustCheckBox.isSelected())) {
+//            StaticVars.reputationType = 3;
+//        } else if (optionDNSCheckBox.isSelected() && (optionSpesificCheckBox.isSelected()) && (!optionTrustCheckBox.isSelected())) {
+//            StaticVars.reputationType = 4;
+//        } else if (optionDNSCheckBox.isSelected() && (!optionSpesificCheckBox.isSelected()) && (optionTrustCheckBox.isSelected())) {
+//            StaticVars.reputationType = 5;
+//        } else if (optionDNSCheckBox.isSelected() && (optionSpesificCheckBox.isSelected()) && (!optionTrustCheckBox.isSelected())) {
+//            StaticVars.reputationType = 6;
+//        } else {
+//            StaticVars.reputationType = 7;
+//        }
+//
+//        // Go into Statistic screens
+//        Parent root = null;
+//        try {
+//            root = FXMLLoader.load(getClass().getResource("statistik.fxml"));
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        Stage stage = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
+//        stage.setScene(new Scene(root));
+//    }
 
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        malwareRadioButton.setToggleGroup(siteListRadioButton);
-        phishingRadioButton.setToggleGroup(siteListRadioButton);
-        spammingRadioButton.setToggleGroup(siteListRadioButton);
-        popularRadioButton.setToggleGroup(siteListRadioButton);
-        malwareRadioButton.setSelected(true);
+//        malwareRadioButton.setToggleGroup(siteListRadioButton);
+//        phishingRadioButton.setToggleGroup(siteListRadioButton);
+//        spammingRadioButton.setToggleGroup(siteListRadioButton);
+//        popularRadioButton.setToggleGroup(siteListRadioButton);
+//        malwareRadioButton.setSelected(true);
+        
+    }
+
+    public void handleDiagnoseButton(ActionEvent actionEvent) {
+        
+    }
+
+    public void handleHistoryButton(ActionEvent actionEvent) {
+
     }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,8 +11,10 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("css/main_page.css").toExternalForm());
         primaryStage.setTitle("Training Data Statistics");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
